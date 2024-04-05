019f4dcd0bab52e5e80208f6217a550ceec62b92
Elielson Rodrigues <ElielsonRodrigues@users.noreply.github.com>
2024-04-05 01:15:33 -0300

    Limpando alguns comentarios

--- src/main/java/br/elielsonRodrigues/git/clientesapi/ClienteApiApplication.java
+++ src/main/java/br/elielsonRodrigues/git/clientesapi/ClienteApiApplication.java
@@ -5,15 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ClienteApiApplication {
-
-	/*
-	@Bean 
-	CommandLineRunner run(@Autowired ClienteRepository clienteRepository) {
-		return args -> {
-			Cliente cliente =  Cliente.builder().cpf("02195650230").nome("ELIELSON").build();
-			clienteRepository.save(cliente);
-		};
-	}*/
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ClienteApiApplication.class, args);
